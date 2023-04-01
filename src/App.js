53e1523a09966d6f309572027cdca74736157706
Ecem Kalınsaz <ecemm.basmaci@gmail.com>
2023-04-01 17:50:43 +0300

    talimatlar tamamlandı

--- src/App.js
+++ src/App.js
@@ -1,8 +1,23 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Karakter from "./components/Karakter"
 
 const App = () => {
+  const [karakterler, setKarakterler] = useState([]); 
   // Try to think through what state you'll need for this app before starting. Then build out
   // the state properties here.
+  useEffect(() => {
+    axios.get('https://swapi.dev/api/people/', {
+    })
+    .then(function (response) {
+      // response.data = Karakter listesi
+      // `.data` bir kaliptir. Her zaman API'nin cevabina bu sekilde erisiriz.
+      setKarakterler(response.data) // 2. Gorev
+    })
+    .catch(function (error) {
+      console.log(error);
+    })
+  }, [])
 
   // Fetch characters from the API in an effect hook. Remember, anytime you have a 
   // side effect in a component, you want to think about which state and/or props it should
@@ -11,6 +26,12 @@ const App = () => {
   return (
     <div className="App">
       <h1 className="Header">Karakterler</h1>
+      <div className='accordion-div'>
+      {karakterler.map(kisi => {
+        return <div className='accordion'><Karakter kisi={kisi} /></div>
+      })}
+      </div>
+
     </div>
   );
 }
--- src/components/Karakter.js
+++ src/components/Karakter.js
@@ -1,1 +1,49 @@
-// Karakter bileşeniniz buraya gelecek
+import React, { useEffect, useState } from 'react';
+import axios from 'axios';
+import Accordion from '@mui/material/Accordion';
+import AccordionSummary from '@mui/material/AccordionSummary';
+import AccordionDetails from '@mui/material/AccordionDetails';
+import Typography from '@mui/material/Typography';
+
+export default function Karakter(props) {
+    console.log(props.kisi, ' PROPS')
+
+    const [gezegen, setGezegen] = useState(''); 
+
+    const kisi = props.kisi
+
+    useEffect(() => {
+        axios.get(kisi.homeworld, {
+        params: {
+        }
+        })
+        .then(function (response) {
+        setGezegen(response.data.name)
+        })
+        .catch(function (error) {
+        console.log(error);
+        })
+    }, [])
+
+    return (
+       <Accordion className='accordion-item'>
+        <AccordionSummary
+          aria-controls="panel1a-content"
+          id="panel1a-header"
+        >
+          <Typography>{ kisi.name }</Typography>
+        </AccordionSummary>
+        <AccordionDetails>
+          <Typography>
+            <p><strong>gender:</strong> {kisi.gender}</p>
+            <p><strong>hair color:</strong> {kisi.hair_color}</p>
+            <p><strong>height:</strong> {kisi.height}</p>
+            <p><strong>homeworld:</strong> { gezegen }</p>
+            <p><strong>mass: </strong>{kisi.mass}</p>
+            <p><strong>skin color:</strong> {kisi.skin_color}</p>
+            <p><strong>movies: </strong>{ kisi.films.join(', ') }</p>
+         </Typography>
+        </AccordionDetails>
+      </Accordion>
+    )
+}
